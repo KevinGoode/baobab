229a698c8c5a683b6cc90dd848a7bb4af79a2145
Kevin Goode <drkevgoode@hotmail.com>
2018-03-20 20:57:07 +0000

    Improved  e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,37 @@ describe('my-app App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display home view', () => {
+    page.navigateTo('/');
+    //Make sure these main elements displayed
+    expect(page.getTopPanel()).toEqual(1);
+    expect(page.getDashboard()).toEqual(1);
+    //Make sure these main elements NOT displayed
+    expect(page.getAppFilesView()).toEqual(0);
+    expect(page.getAppFilesTree()).toEqual(0);
+    expect(page.getAppFileDetail()).toEqual(0);
+    //Detailed checks
+    expect(page.getTopPanelText()).toEqual('BaoBab');
+    expect(page.getTopPanelButtonCount()).toEqual(2);
+    expect(page.getTopPanelSplitButtonCount()).toEqual(2);
+    expect(page.getAttributeOfTopPanelButton('Home','icon')).toEqual('fa fa-home');
+    expect(page.getAttributeOfTopPanelButton('Articles','icon')).toEqual('fa fa-file-text');
+    expect(page.getAttributeOfTopPanelSplitButton('Login','icon')).toEqual('fa-user');
+    expect(page.getAttributeOfTopPanelSplitButton('Help','icon')).toEqual('fa-book');
+
+
   });
+
+  it('should display files view', () => {
+    page.navigateTo('/files');
+    //Make sure these main elements displayed
+    expect(page.getTopPanel()).toEqual(1);
+    expect(page.getAppFilesView()).toEqual(1);
+    expect(page.getAppFilesTree()).toEqual(1);
+    expect(page.getAppFileDetail()).toEqual(1);
+    //Make sure these main elements NOT displayed
+    expect(page.getDashboard()).toEqual(0);
+    //Detailed checks
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,42 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element , promise} from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(url:string) {
+    return browser.get(url);
   }
 
-  getParagraphText() {
+  //Get components
+  getDashboard() {
+    return element.all(by.css('app-dashboard')).count();
+  }
+  getTopPanel() {
+    return element.all(by.css('app-top-panel')).count();
+  }
+  getAppFilesView(){
+    return element.all(by.css('app-files-view')).count();
+  }
+  getAppFilesTree(){
+    return element.all(by.css('app-files-view app-files-tree')).count();
+  }
+  getAppFileDetail(){
+    return element.all(by.css('app-files-view app-file-detail')).count();
+  }
+
+ //Get component detail
+  getTopPanelText() {
     return element(by.css('app-root h1')).getText();
   }
+  getTopPanelButtonCount(){
+    return element.all(by.css('app-top-panel p-button')).count();
+  }
+  getTopPanelSplitButtonCount(){
+    return  element.all(by.css('app-top-panel p-splitbutton')).count();
+  }
+  getAttributeOfTopPanelButton(buttName:string, attName:string){
+    return  element(by.css('app-top-panel p-button[label=' + buttName + ']')).getAttribute(attName);
+  }
+  getAttributeOfTopPanelSplitButton(buttName:string, attName:string){
+    return  element(by.css('app-top-panel p-splitbutton[label=' + buttName + ']')).getAttribute(attName);
+  }
+ 
 }
